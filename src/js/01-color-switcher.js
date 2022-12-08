670a24e667a010d2f171f5b698e2df0b3b54d369
Rost Redvanskyi <redvanskyirost@gmail.com>
2022-12-08 16:59:44 +0200

    add 1-st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timeIntervalId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  timeIntervalId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timeIntervalId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
