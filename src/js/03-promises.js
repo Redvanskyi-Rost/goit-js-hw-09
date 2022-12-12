86637e628638bbca4e2830528644b43d88398a8a
Rost Redvanskyi <redvanskyirost@gmail.com>
2022-12-12 16:58:31 +0200

    дз-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
+import Notiflix from 'notiflix';
+
+const formRef = document.querySelector('form');
+
+formRef.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  let delay = Number(formRef[0].value);
+  let step = Number(formRef[1].value);
+  let amount = Number(formRef[2].value);
+
+  if (delay <= 0 || step <= 0 || amount <= 0) {
+    Notiflix.Notify.info('Enter a positive numbers.');
+    return;
+  }
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delay += step;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
